43d9741607c1b2f8059da46290dca3f59ac076d9
Vathsav <vathsav@live.in>
2019-05-05 15:40:55 +0200

    feat: created content preview on admin panel

--- src/modules/admin/index.js
+++ src/modules/admin/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 class AdminPanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      previewText: '',
+    };
+
+    this.updatePreview = this.updatePreview.bind(this);
+  }
+
+  updatePreview(e) {
+    this.setState({
+      previewText: e.target.value,
+    });
+  }
+
   render() {
+    const { previewText } = this.state;
     return (
-      <Container fluid className="bg-red-light">
-        admin panel
+      <Container fluid>
+        <Row>
+          <Col sm={12} md={6}>
+            {/* Fields */}
+            <textarea name="" id="" rows={10} className="w-100" placeholder="Content" onChange={this.updatePreview} />
+          </Col>
+
+          <Col sm={12} md={6}>
+            {/* Preview */}
+            <div className="card w-100" dangerouslySetInnerHTML={{ __html: previewText }} />
+          </Col>
+
+        </Row>
       </Container>
     );
   }
